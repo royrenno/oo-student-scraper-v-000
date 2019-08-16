daf6588829ab3267b748e1f6fdd535da2b0eba4f
roy renno <royrenno@yahoo.com>
2019-08-16 00:55:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,12 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
     student_hash.each {|key, value| self.send(("#{key}="),value)}
-    @@all << self    
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
@@ -14,11 +14,10 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
   def self.all
-    
+
   end
 end
-
