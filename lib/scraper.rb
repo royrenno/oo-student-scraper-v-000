504d3fcd4def76655fca358f8d04b56e1059bd70
roy renno <royrenno@yahoo.com>
2019-08-16 00:43:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,4 +22,29 @@ doc.css('.student-card').each do |students|
     end
   def self.scrape_profile_page(profile_url)
 
+    doc = Nokogiri::HTML(open(profile_url))
+    student = {}
+
+     social_container = doc.css(".social-icon-container a").collect {|icon| icon.attribute("href").value}
+
+       social_container.each do |link|
+        if link.include?("twitter")
+          student[:twitter] = link
+        elsif link.include?("linkedin")
+          student[:linkedin] = link
+        elsif link.include?("github")
+          student[:github] = link
+        else
+          student[:blog] = link
+        end
+      end
+
+       student[:bio] = doc.css('.description-holder p').text
+      student[:profile_quote] = doc.css('.profile-quote').text
+    student
+    end
+
   end
+
+
+
